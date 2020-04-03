0103f2df7bd02fa4bf6d9e7f5f4908260409ccef
Dor494 <dor494@inlumine.ual.es>
2020-04-03 16:49:12 +0200

    Sincronizacion

--- foro/src/main/java/com/mds/foro/Crear_tema.java
+++ foro/src/main/java/com/mds/foro/Crear_tema.java
@@ -1,8 +1,8 @@
 package com.mds.foro;
 
 public class Crear_tema {
-	private JTextField _tituloTemaTF;
-	private JTextField _subtituloTemaTF;
+	private TextField _tituloTemaTF;
+	private TextField _subtituloTemaTF;
 	private event _guardarTema;
 	public Visualizar_seccion__Usuario_identificado_ _unnamed_Visualizar_seccion__Usuario_identificado__;
 	public Validar_tema _validar_tema;
