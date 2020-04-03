9a3191868e103829c471b9773f0cbb0c2dedcbde
Dor494 <dor494@inlumine.ual.es>
2020-04-03 16:53:42 +0200

    S3

--- foro/src/main/java/com/mds/foro/Cambiar_contraseña.java
+++ foro/src/main/java/com/mds/foro/Cambiar_contraseña.java
@@ -1,15 +1,15 @@
 package com.mds.foro;
 
-public class Cambiar_contrase�a {
+public class Cambiar_contrasena {
 	private Label _tituloL;
-	private TextField _ingreseContrase�aTF;
-	private JTextField _repetirContrase�aTF;
-	private JLabel _cancelarL;
-	private JButton _cancelarB;
-	private JLabel _confirmarL;
-	private JButton _confirmarB;
+	private TextField _ingreseContrasenaTF;
+	private TextField _repetirContrasenaTF;
+	private Label _cancelarL;
+	private Button _cancelarB;
+	private Label _confirmarL;
+	private Button _confirmarB;
 	public Visualizar_mi_perfil _unnamed_Visualizar_mi_perfil_;
-	public Validar_contrase�a _validar_contrase�a;
+	public Validar_contrasena _validar_contrasena;
 
 	public void confirmar_cambio_password() {
 		throw new UnsupportedOperationException();
