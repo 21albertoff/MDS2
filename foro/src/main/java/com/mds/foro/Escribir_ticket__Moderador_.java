cd8278186c1b30c9f15c40e6b262f2626a264155
Dor494 <dor494@inlumine.ual.es>
2020-04-06 16:21:22 +0200

    Subida de cambios

--- foro/src/main/java/com/mds/foro/Escribir_ticket__Moderador_.java
+++
@@ -1,6 +0,0 @@
-package com.mds.foro;
-
-public class Escribir_ticket__Moderador_ extends Escribir_ticket {
-	private JLabel _ticketL;
-	private JButton _ticketB;
-}
--- foro/src/main/java/com/mds/foro/Escribir_ticket__Usuario_registrado_.java
+++
@@ -1,7 +0,0 @@
-package com.mds.foro;
-
-public class Escribir_ticket__Usuario_registrado_ extends Escribir_ticket {
-	private JLabel _ticketL;
-	private JButton _ticketB;
-	public Usuario_registrado _unnamed_Usuario_registrado_;
-}
