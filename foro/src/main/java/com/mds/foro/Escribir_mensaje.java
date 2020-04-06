29ab8132f1c291f4107234213a2fbdd3f2726e49
Alberto Fuentes <admin@example.com>
2020-04-06 16:01:43 +0200

    Colocando menú en las vistas

--- foro/src/main/java/com/mds/foro/Escribir_mensaje.java
+++ foro/src/main/java/com/mds/foro/Escribir_mensaje.java
@@ -1,17 +1,17 @@
 package com.mds.foro;
 
 public class Escribir_mensaje {
-	private event _añadir_fotos;
-	private event _añadir_video;
+	private event _aï¿½adir_fotos;
+	private event _aï¿½adir_video;
 	private JTextField _mensajeTF;
 	public Responder_mensaje _responder_mensaje;
 	public Visualizar_tema_y_mensajes__Usuario_identificado_ _unnamed_Visualizar_tema_y_mensajes__Usuario_identificado__;
 
-	public void añadir_fotos() {
+	public void add_fotos() {
 		throw new UnsupportedOperationException();
 	}
 
-	public void añadir_video() {
+	public void aï¿½adir_video() {
 		throw new UnsupportedOperationException();
 	}
 }
