1ee73a2082d6451d78b14f32a0cc36474bcac13b
Dor494 <dor494@inlumine.ual.es>
2020-06-10 17:20:24 +0200

    Arreglo de notificacion

--- foro/src/main/java/com/mds/foro/DB_Notificaciones.java
+++ foro/src/main/java/com/mds/foro/DB_Notificaciones.java
@@ -55,10 +55,10 @@ public class DB_Notificaciones {
 			NotificacionDB notificacion = NotificacionDBDAO.createNotificacionDB();
 			Usuario_DB user = Usuario_DBDAO.loadUsuario_DBByORMID(idUsuarioAmigo);
 			Usuario_DB user2 = Usuario_DBDAO.loadUsuario_DBByORMID(idUsuario);
-			notificacion.setEnviada_por(user);
+			notificacion.setEnviada_por(user2);
 			NotificacionDBDAO.save(notificacion);
-			user2.recibe.add(notificacion);
-			Usuario_DBDAO.save(user2);
+			user.recibe.add(notificacion);
+			Usuario_DBDAO.save(user);
 			t.commit();
 		} catch (Exception e) {
 			t.rollback();
